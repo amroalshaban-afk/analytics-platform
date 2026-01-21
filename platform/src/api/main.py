9fa12fdd398b2d7b16ebabf3fb9a6f7ebb9307ab
MathoVerse100 <224757964+amroalshaban-afk@users.noreply.github.com>
2026-01-21 22:52:48 +0300

    Fix: added 'ENVIRONMENT' variable to hide dev urls (such as /docs) in prod

--- platform/src/api/main.py
+++ platform/src/api/main.py
@@ -1,9 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
 from api.routes.endpoints import paths as api_paths
+import os
 
-
-app = FastAPI()
+app = FastAPI(
+    docs_url="/documentation" if os.getenv('ENVIRONMENT') == 'dev' else None,
+    redoc_url="/redocly" if os.getenv('ENVIRONMENT') == 'dev' else None,
+    openapi_url="/openapi" if os.getenv('ENVIRONMENT') == 'dev' else None,
+)
 for api_path in api_paths:
     api_path(app)
 
