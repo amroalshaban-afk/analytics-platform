db5d59e0ba667164b2a39146ea9bbf94ee1f1518
MathoVerse100 <224757964+amroalshaban-afk@users.noreply.github.com>
2026-01-21 15:28:22 +0300

    Refactor: changed docker-compose prod setup + github actions

--- platform/src/api/routes/base.py
+++ platform/src/api/routes/base.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from api.utils.func1 import add
+from utils.func1 import add
 
 from services.tasks import app as celery_app
 
@@ -8,6 +8,4 @@ def generator(app: FastAPI):
 
     @app.get('/')
     async def endpoint1():
-        celery_app.send_task('jobs.add', args=(1, 2))
-
         return {"endpoint1": f"Hello, MOM! {add(1, 1)}"}
