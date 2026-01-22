06998e50bda98bdf4917faf05e6d9872784eb9c1
MathoVerse100 <224757964+amroalshaban-afk@users.noreply.github.com>
2026-01-22 09:24:35 +0300

    Fix: appended slack commands with '-dev' flag to differentiate dev from prod

--- platform/src/api/routes/slack/slash_commands/hello.py
+++ platform/src/api/routes/slack/slash_commands/hello.py
@@ -1,11 +1,13 @@
+import os
 from slack_bolt.async_app import AsyncApp
 # from services.tasks import app as celery_app
 
+ENVIRONMENT = os.getenv('ENVIRONMENT')
 
 def generator(app: AsyncApp):
 
     if app.name in ('VIP-Baloot', 'Awad-Delivery'):
-        @app.command(f'/hello-{app.name.lower()}')
+        @app.command(f'/hello-{app.name.lower()}{f'-{ENVIRONMENT}' if ENVIRONMENT == 'dev' else None}')
         async def hello(ack, respond, response_url, command):
             await ack()
             await respond(text="Hello, MOM!", response_type='in_channel')
